ecc13c15b7ec360dc49090b23c6c697a53456fa1
fernando <fernandoluppo2@gmail.com>
2024-02-17 15:58:11 -0300

    feat: created auth hook

--- src/hooks/useAuth.ts
+++ src/hooks/useAuth.ts
@@ -1,4 +1,33 @@
-export const get = async () => {}
-export const post = async () => {}
-export const patch = async () => {}
-export const remove = async () => {}
+import { api } from "@/service"
+
+export const get = async (url: string) => {
+  try {
+    return await api.get(url)
+  } catch (error) {
+    console.log(error)
+  }
+}
+
+export const post = async (url: string, data: unknown) => {
+  try {
+    return await api.post(url, data)
+  } catch (error) {
+    console.log(error)
+  }
+}
+
+export const patch = async (url: string, data: unknown) => {
+  try {
+    return await api.patch(url, data)
+  } catch (error) {
+    console.log(error)
+  }
+}
+
+export const remove = async (url: string) => {
+  try {
+    return await api.delete(url)
+  } catch (error) {
+    console.log(error)
+  }
+}
